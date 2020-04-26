5ad31dac34032fc700ad11547545baeba091d196
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-04-26 11:06:56 +0300

    Plumbing
    Build system

---
+++ build/templates/link.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  @package   DarkMagic
+ *  @copyright Copyright (c)2019-2020 Nicholas K. Dionysopoulos
+ *  @license   GNU General Public License version 3, or later
+ */
+
+$hardlink_files = array(
+);
+
+$symlink_files = array(
+);
+
+$symlink_folders = array(
+);
